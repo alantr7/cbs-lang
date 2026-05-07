e51d15cc01fb223811e8b7e45669540741fbad93
alantr7 <alan.trumic066@gmail.com>
2026-05-07 15:50:57 +0200

    fix: missing precedence for "&" and "|" operators

--- src/main/java/com/github/alantr7/codebots/cbslang/high/parser/ParserHelper.java
+++ src/main/java/com/github/alantr7/codebots/cbslang/high/parser/ParserHelper.java
@@ -60,6 +60,8 @@ public class ParserHelper {
             case "="  -> 2; // maybe above NEEDS to be 1. check if breaks
             case "||" -> 3;
             case "&&" -> 4;
+            case "|" -> 5;
+            case "&" -> 6;
             case "<", ">", "==", "!=", "<=", ">=" -> 7;
             case "+", "-" -> 8;
             case "*", "/" -> 9;
